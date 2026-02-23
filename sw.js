8cce165001f105219bc8487c664d011782887fb7
fsonline68 <fsonline68@gmail.com>
2026-02-23 19:04:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-const CACHE_NAME = 'zeit-app-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res |
-
-| fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
